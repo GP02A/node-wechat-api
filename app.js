f1b4ca00ad463bbdd9312cae84ce8f2d56902592
Bo <zealot37@gmail.com>
2018-01-15 16:46:25 +0800

    export jsapi function from app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const getToken = require('./lib/token');
 getToken.fulltokenrefresh();
 
 const JSAPI = require('./lib/wxjssdk');
-
+exports.cjsconfig = JSAPI.cjsconfig;
 /*创建菜单
 const createMenu = require('./wechat/wxCustomeMenu');
 createMenu();
