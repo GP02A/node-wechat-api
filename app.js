5124421019f99b27a8690e9f2fc01998ab5b7aca
Bo <zealot37@gmail.com>
2018-01-15 15:31:38 +0800

    prototype

---
+++ app.js
@@ -0,0 +1,15 @@
+// load the things we need
+var fs = require('fs');
+const crypto = require('crypto');
+//const wxconfig = require('./config');
+
+//刷新timeStamp, signature, access_token
+const getToken = require('./lib/token');
+getToken.fulltokenrefresh();
+
+const JSAPI = require('./lib/wxjssdk');
+
+/*创建菜单
+const createMenu = require('./wechat/wxCustomeMenu');
+createMenu();
+*/
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'token': 'weixin',
+  'appId': 'wx6b22481b8c06c33c',
+  'appSecret': '35a26cc446fd39bba8ca2668c319a853',
+};
